cd94add66a8690562d021b30bb775c660bb5e8c8
enadreva <reva.enad@jg-corporation.com>
2024-07-23 17:11:01 +0800

    added resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
                     <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100
                     group-hover:-inset-1 group-hover:duration-200 animate-tilt">
                     </div>
-                    <a href='/' title='Download CV' role='button' 
+                    <a href='/EnadReva_Resume.pdf' title='Download CV' role='button' 
                         className="download-cv-button w-[190px] h-[60px] relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold
                          text-white transition-all duration-200 bg-primary-color rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
                         Download CV
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,28 @@ const Navbar = () => {
     const handleNav = () => {
         setNav(!nav)
     }
+    
+    const navLinks = [
+        {href:'#home', label: 'Home'},
+        {href:'#about', label:'About'},
+        {href:'#work', label:'Work'},
+        {href:'#experience', label:'Experience'},
+        {href:'#contact', label:'Contact'},
+    ];
 
   return (
     <div className='z-10 text-gray-500 flex justify-between items-center max-w-[1240px] mx-auto h-24 px-4 text-l'>
 
         <h1 className='text-3xl font-bold primary-color ml-4'>REVA.E</h1>
         <ul className='hidden md:flex '>
-            <li className='p-2'><a href='#home'>Home</a></li>
-            <li className='p-2'><a href='#about'>About</a></li>
-            <li className='p-2'><a href='#work'>Work</a></li>
-            <li className='p-2'><a href='#experience'>Experience</a></li>
-            <li className='p-2'><a href='#experience'>Contact</a></li>
+            <li className='p-5'>
+                {navLinks.map((link) =>
+                (
+                    <a key={link.href} className='p-5' href={link.href}>
+                        {link.label}
+                    </a>
+                ))}
+            </li>
         </ul>
 
     <div onClick={handleNav} className='block md:hidden z-20'>
@@ -28,10 +39,14 @@ const Navbar = () => {
                         : 'fixed left-[-100%] z-20'}>
         <h1 className='text-3xl font-bold primary-color m-4'>REVA.E</h1>
         <ul className='p-8 text-2xl'>
-            <li className='p-2'>Home</li>
-            <li className='p-2'>About</li>
-            <li className='p-2'>Work</li>
-            <li className='p-2'>Contact</li>
+            <li className='p-2'>
+            {navLinks.map((link) =>
+                (
+                    <a key={link.href} className='p-2' href={link.href}>
+                        {link.label}
+                    </a>
+                ))}
+            </li>
         </ul>
     </div>
     
